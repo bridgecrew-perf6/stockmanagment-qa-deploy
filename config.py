4a3dafb68e48d36844b61344f24c9951195ac45d
mmaina48 <mmaina48@gmail.com>
2022-04-01 13:52:23 +0300

    Use Config class to configure my app

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 from datetime import timedelta
-from application import app
+# from application import app
 
-app.config['SQLALCHEMY_DATABASE_URI'] =os.environ.get('DATABASE_URL')
-app.config['SECRET_KEY'] =os.environ.get('SECRET')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_DATABASE_URI'] =os.environ.get('DATABASE_URL')
+# app.config['SECRET_KEY'] =os.environ.get('SECRET')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# class Config(object):
-#     SECRET_KEY= os.environ.get('SECRET')
-#     # SQLALCHEMY_DATABASE_URI= 'sqlite:///teststockdb.sqlite3'
-#     SQLALCHEMY_DATABASE_URI= os.environ.get('DATABASE_URL')
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+class Config(object):
+    SECRET_KEY= os.environ.get('SECRET')
+    # SQLALCHEMY_DATABASE_URI= 'sqlite:///teststockdb.sqlite3'
+    SQLALCHEMY_DATABASE_URI= os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
