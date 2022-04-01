e9e1198a1b6bc39c4e75f2c0bea108a9b466a900
mmaina48 <mmaina48@gmail.com>
2022-04-01 13:12:08 +0300

    Change database configurations

--- config.py
+++ config.py
@@ -2,8 +2,6 @@ import os
 from datetime import timedelta
 
 class Config(object):
-    SECRET_KEY= os.environ.get('SECRET_KEY') or "secrete_string"
+    SECRET_KEY= os.environ.get('SECRET')
     # SQLALCHEMY_DATABASE_URI= 'sqlite:///teststockdb.sqlite3'
-    SQLALCHEMY_DATABASE_URI= 'postgres://imwosyeftskruk:f9665cf0ad6ae9db056912da7a7de00f6f5f4eac53b56e12da8853b1770d6f6d@ec2-52-3-60-53.compute-1.amazonaws.com:5432/dchfk1gftccen9'
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    PERMANENT_SESSION_LIFETIME =  timedelta(minutes=10)
+    SQLALCHEMY_DATABASE_URI= os.environ.get('DATABASE_URL')
