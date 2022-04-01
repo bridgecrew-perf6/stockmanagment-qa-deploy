3a6093f2db261232bcb66f674b158318fdba501c
mmaina48 <mmaina48@gmail.com>
2022-04-01 13:15:56 +0300

    Set SQLALCHEMY_TRACK_MODIFICATIONS to False

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ class Config(object):
     SECRET_KEY= os.environ.get('SECRET')
     # SQLALCHEMY_DATABASE_URI= 'sqlite:///teststockdb.sqlite3'
     SQLALCHEMY_DATABASE_URI= os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
